58e3c9f1384bb1d096050966204bf9a0a05493f9
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-05-01 18:01:29 -0400

    Completamos el presenter y el index de metricas para la funcion Obtener Cobertura

--- src/presenter.js
+++ src/presenter.js
@@ -12,20 +12,23 @@ const form = document.querySelector("#calcular-form");
 const tablaDatosBody = document.querySelector("#datos-ingresados-body");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const cantidad = document.querySelector("#cantidad").value;
-  const cantidadPruebas = document.querySelector("#cantidad-pruebas").value;
-  const cantidadLineas = document.querySelector("#cantidad-lineas").value;
-  ObtenerCantidadCommits(cantidad);
-  ObtenerCantidadPruebas(cantidadPruebas);
-  ObtenerCantidadLineas(cantidadLineas);
-  const newRow = document.createElement("tr");
-  newRow.innerHTML = `
-      <td>${cantidad}</td>
-      <td>${cantidadPruebas}</td>
-      <td>${cantidadLineas}</td>
-  `;
-  tablaDatosBody.appendChild(newRow);
+    event.preventDefault();
+    const cantidad = document.querySelector("#cantidad").value;
+    const cantidadPruebas = document.querySelector("#cantidad-pruebas").value;
+    const cantidadLineas = document.querySelector("#cantidad-lineas").value;
+    const cobertura = document.querySelector("#cobertura").value;
+    ObtenerCantidadCommits(cantidad);
+    ObtenerCantidadPruebas(cantidadPruebas);
+    ObtenerCantidadLineas(cantidadLineas);
+    ObtenerCobertura(cobertura);
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+       <td>${cantidad}</td>
+       <td>${cantidadPruebas}</td>
+       <td>${cantidadLineas}</td>
+       <td>${cobertura}</td>
+    `;
+    tablaDatosBody.appendChild(newRow);
 });
 
 
