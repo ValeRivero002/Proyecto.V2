7c0c9b9d9d095b4b38877044b68b5901bdad3e9e
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-05-01 22:26:50 -0400

    Aumentamos metricaPresenter para evitar colisiones en el 'submit'

---
+++ src/metricaPresenter.js
@@ -0,0 +1,72 @@
+import { ObtenerCantidadCommits, ObtenerCantidadPruebas, ObtenerCantidadLineas, ObtenerCobertura} from "./tdd.js";
+import { obtenerPuntajeTotalPorCommit, obtenerRetroalimentacionPorPuntajePruebas, obtenerRetroalimentacionPorPuntajeLineas, obtenerRetroalimentacionPorCobertura, obtenerPuntajePorCantidadPruebas, obtenerPuntajePorCantidadLineas, obtenerPuntajePorCobertura} from "./totalizador.js";
+
+
+const puntajeTotalProyectoDiv = document.getElementById('puntaje-total-proyecto');
+
+let sumaPuntajesTotales = 0;
+
+//METRICAS
+const form = document.querySelector("#calcular-form");
+const tablaDatosBody = document.querySelector("#datos-ingresados-body");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nombre = document.querySelector("#nombre").value;
+  const cantidad = parseInt(document.querySelector("#cantidad").value);
+  const cantidadPruebas = parseInt(document.querySelector("#cantidad-pruebas").value);
+  const cantidadLineas = parseInt(document.querySelector("#cantidad-lineas").value);
+  const cobertura = parseFloat(document.querySelector("#cobertura").value);
+  
+
+  // Obtener los valores de las métricas
+  const commits = ObtenerCantidadCommits(cantidad);
+  const pruebas = ObtenerCantidadPruebas(cantidadPruebas);
+  const lineas = ObtenerCantidadLineas(cantidadLineas);
+  const cov = ObtenerCobertura(cobertura);
+
+//Calcular los puntajes de los commits
+const puntajePruebas = obtenerPuntajePorCantidadPruebas(pruebas);
+const puntajeLineas = obtenerPuntajePorCantidadLineas(lineas);
+const puntajeCobertura = obtenerPuntajePorCobertura(cov);
+
+  // Calcular el puntaje total por commit
+const puntajeTotal = obtenerPuntajeTotalPorCommit(puntajeLineas, puntajePruebas, puntajeCobertura);
+
+// Obtener la retroalimentación para cada puntaje
+const retroalimentacionPruebas = obtenerRetroalimentacionPorPuntajePruebas(puntajePruebas);
+const retroalimentacionLineas = obtenerRetroalimentacionPorPuntajeLineas(puntajeLineas);
+const retroalimentacionCobertura = obtenerRetroalimentacionPorCobertura(puntajeCobertura);
+
+// Calcula la suma de los puntajes totales
+sumaPuntajesTotales += puntajeTotal;
+
+// Calcula el promedio de los puntajes totales
+const cantidadFilas = tablaDatosBody.children.length + 1;
+const promedioPuntajesTotales = sumaPuntajesTotales / cantidadFilas;
+
+// Muestra el puntaje total del proyecto en el div correspondiente
+puntajeTotalProyectoDiv.textContent = `Puntaje total del proyecto: ${promedioPuntajesTotales}`;
+
+// Crear una nueva fila en la tabla con los valores de las métricas y el puntaje total
+const newRow = document.createElement("tr");
+newRow.innerHTML = `
+     <td>${nombre}</td>
+     <td>${commits}</td>
+     <td>${pruebas}</td>
+     <td>${lineas}</td>
+     <td>${cov}</td>
+     <td>${puntajeTotal}</td>
+     <td>${retroalimentacionPruebas}<br>${retroalimentacionLineas}<br>${retroalimentacionCobertura}</td>
+     <td><button class="eliminar-button">Eliminar</button></td>
+`;
+
+// Agregar la nueva fila a la tabla
+tablaDatosBody.appendChild(newRow);
+  // Agregar evento de click al botón de eliminar
+  newRow.querySelector(".eliminar-button").addEventListener("click", () => {
+      tablaDatosBody.removeChild(newRow);
+  });
+
+  
+});
--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,5 @@
-import { agregarProyecto, ObtenerCantidadCommits, ObtenerCantidadPruebas, ObtenerCantidadLineas, ObtenerCobertura, eliminarProyecto } from "./tdd.js";
-import { obtenerPuntajeTotalPorCommit, obtenerRetroalimentacionPorPuntajePruebas, obtenerRetroalimentacionPorPuntajeLineas, obtenerRetroalimentacionPorCobertura, obtenerPuntajePorCantidadPruebas, obtenerPuntajePorCantidadLineas, obtenerPuntajePorCobertura} from "./totalizador.js";
 
-
+import { agregarProyecto } from "./tdd.js";
 
 
 // Obtener referencias a los elementos del DOM
@@ -10,76 +8,6 @@ const agregarProyectoBtn = document.getElementById('agregar-proyecto');
 const cerrarModalBtn = document.querySelector('.close');
 const proyectosLista = document.getElementById('proyectos-lista');
 const agregarProyectoForm = document.getElementById('agregar-proyecto-form');
-const puntajeTotalProyectoDiv = document.getElementById('puntaje-total-proyecto');
-
-let sumaPuntajesTotales = 0;
-
-//METRICAS
-const form = document.querySelector("#calcular-form");
-const tablaDatosBody = document.querySelector("#datos-ingresados-body");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const nombre = document.querySelector("#nombre").value;
-  const cantidad = parseInt(document.querySelector("#cantidad").value);
-  const cantidadPruebas = parseInt(document.querySelector("#cantidad-pruebas").value);
-  const cantidadLineas = parseInt(document.querySelector("#cantidad-lineas").value);
-  const cobertura = parseFloat(document.querySelector("#cobertura").value);
-  
-
-  // Obtener los valores de las métricas
-  const commits = ObtenerCantidadCommits(cantidad);
-  const pruebas = ObtenerCantidadPruebas(cantidadPruebas);
-  const lineas = ObtenerCantidadLineas(cantidadLineas);
-  const cov = ObtenerCobertura(cobertura);
-
-//Calcular los puntajes de los commits
-const puntajePruebas = obtenerPuntajePorCantidadPruebas(pruebas);
-const puntajeLineas = obtenerPuntajePorCantidadLineas(lineas);
-const puntajeCobertura = obtenerPuntajePorCobertura(cov);
-
-  // Calcular el puntaje total por commit
-const puntajeTotal = obtenerPuntajeTotalPorCommit(puntajeLineas, puntajePruebas, puntajeCobertura);
-
-// Obtener la retroalimentación para cada puntaje
-const retroalimentacionPruebas = obtenerRetroalimentacionPorPuntajePruebas(puntajePruebas);
-const retroalimentacionLineas = obtenerRetroalimentacionPorPuntajeLineas(puntajeLineas);
-const retroalimentacionCobertura = obtenerRetroalimentacionPorCobertura(puntajeCobertura);
-
-// Calcula la suma de los puntajes totales
-sumaPuntajesTotales += puntajeTotal;
-
-// Calcula el promedio de los puntajes totales
-const cantidadFilas = tablaDatosBody.children.length + 1;
-const promedioPuntajesTotales = sumaPuntajesTotales / cantidadFilas;
-
-// Muestra el puntaje total del proyecto en el div correspondiente
-puntajeTotalProyectoDiv.textContent = `Puntaje total del proyecto: ${promedioPuntajesTotales}`;
-
-// Crear una nueva fila en la tabla con los valores de las métricas y el puntaje total
-const newRow = document.createElement("tr");
-newRow.innerHTML = `
-     <td>${nombre}</td>
-     <td>${commits}</td>
-     <td>${pruebas}</td>
-     <td>${lineas}</td>
-     <td>${cov}</td>
-     <td>${puntajeTotal}</td>
-     <td>${retroalimentacionPruebas}<br>${retroalimentacionLineas}<br>${retroalimentacionCobertura}</td>
-     <td><button class="eliminar-button">Eliminar</button></td>
-`;
-
-// Agregar la nueva fila a la tabla
-tablaDatosBody.appendChild(newRow);
-  // Agregar evento de click al botón de eliminar
-  newRow.querySelector(".eliminar-button").addEventListener("click", () => {
-      tablaDatosBody.removeChild(newRow);
-  });
-
-  
-});
-
-
 
 // Función para mostrar el modal
 const mostrarModal = () => modal.style.display = 'block';
